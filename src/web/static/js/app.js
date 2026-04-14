b73393f5b443dfc45b97f6c4614c539c02d0dea9
Elisha-Onyando <elisha.onyando23@gmail.com>
2026-04-14 17:23:04 +0300

    Second commit

--- src/web/static/js/app.js
+++ src/web/static/js/app.js
@@ -1,12 +1,12 @@
-async function createWebhook(){
-    const res = await fetch(API.webhooks, {
-        method: "POST"
-    });
+async function createWebhook() {
+  const res = await fetch(API.webhooks, {
+    method: "POST",
+  });
 
-    const data = await res.json();
-    const webhook_id = data.webhook_url.split("/").pop();
+  const data = await res.json();
+  const webhook_id = data.webhook_url.split("/").pop();
 
-    document.getElementById("result").innerHTML = `
+  document.getElementById("result").innerHTML = `
         <div>
             <h3 class="fs-4">Webhook Created</h3>
             <div class="p-1 mx-auto mt-4 text-primary bg-primary-subtle border border-primary rounded-3">
@@ -14,10 +14,12 @@ async function createWebhook(){
             </div>
             <br/>
             <hr/>
-            <a href="/webhooks/${webhook_id}"
-                class="text-decoration-none mt-3 p-2 justify-content-center mx-auto bg-outline-primary text-center border border-primary rounded-3">
-                View Requests
-            </a>
+            <div class="mt-3 text-center">
+                <a href="/webhooks/${webhook_id}"
+                    class="text-decoration-none p-2 mx-auto bg-outline-primary text-center border border-primary rounded-3 d-inline-block">
+                    View Requests
+                </a>
+            </div>
         </div>
     `;
 }
